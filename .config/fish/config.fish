bec486e7edff8d31b0ac33d74e7e279d618f2005
Paul Abrams <john.paul.abrams@gmail.com>
2026-02-01 20:44:56 -0500

    alacritty colors

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,3 @@
 source /usr/share/cachyos-fish-config/cachyos-config.fish
 
-# overwrite greeting
-# potentially disabling fastfetch
-#function fish_greeting
-#    # smth smth
-#end
+alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
