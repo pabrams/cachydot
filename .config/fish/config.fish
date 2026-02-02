aa6e955141f386fefd8c861b7bcf3b8d2f25ad0f
Paul Abrams <john.paul.abrams@gmail.com>
2026-02-02 11:53:38 -0500

    config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,10 @@
 source /usr/share/cachyos-fish-config/cachyos-config.fish
 
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+
+# pnpm
+set -gx PNPM_HOME "/home/i/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
