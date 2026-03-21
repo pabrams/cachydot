293a4198f9da6f09e1977664fea6fd2e3d1e5a2f
Paul Abrams <1376688+pabrams@users.noreply.github.com>
2026-03-21 09:11:49 -0400

    fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,14 +11,15 @@ end
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-if test -f /home/i/miniconda3/bin/conda
-    eval /home/i/miniconda3/bin/conda "shell.fish" "hook" $argv | source
-else
-    if test -f "/home/i/miniconda3/etc/fish/conf.d/conda.fish"
-        . "/home/i/miniconda3/etc/fish/conf.d/conda.fish"
-    else
-        set -x PATH "/home/i/miniconda3/bin" $PATH
-    end
-end
+# if test -f /home/i/miniconda3/bin/conda
+#     eval /home/i/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+# else
+#     if test -f "/home/i/miniconda3/etc/fish/conf.d/conda.fish"
+#         . "/home/i/miniconda3/etc/fish/conf.d/conda.fish"
+#     else
+#         set -x PATH "/home/i/miniconda3/bin" $PATH
+#     end
+# end
 # <<< conda initialize <<<
 
+starship init fish | source
