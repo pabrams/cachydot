9beaaa2b5749b8f9e5f86d3d1fdb7ce0cfb234f1
Paul Abrams <1376688+pabrams@users.noreply.github.com>
2026-08-18 20:58:44 -0400

    config

---
+++ .config/fish/functions/mirror.fish
@@ -0,0 +1,3 @@
+function mirror --wraps='sudo cachyos-rate-mirrors' --wraps='uxplay -p -avdec -vs xvimagesink' --description 'alias mirror=uxplay -p -avdec -vs xvimagesink'
+    uxplay -p -avdec -vs xvimagesink $argv
+end
